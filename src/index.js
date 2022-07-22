f964ec2135c9cc566d116df55b94830ae6a063b0
Alexis <alexisvasquestello@gmail.com>
2022-07-22 18:37:39 -0500

    Changed the format in the @import of the call to the style sheet from .css to .scss

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ sayHi()**/
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './style.css';
+import './style.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container); 
 root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode> 
+    </React.StrictMode>
 );
