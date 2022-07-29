1f61238b4f3bb2e56aeec32364298d7dd4fdc224
Alexis <alexisvasquestello@gmail.com>
2022-07-29 00:14:29 -0500

    Added a container and a row | Bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useRef } from 'react';
 import { FaEllipsisV,FaTimes } from 'react-icons/fa';
+import { Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../assets/logo-alexisDev-azul.png';
 
 
@@ -9,21 +11,25 @@ function Navbar() {
         navRef.current.classList.toggle('responsive_nav');
     }
     return (
-        <header>
-            <div><img src={logo} alt='img'></img></div>
-            <nav ref={navRef}>
-                <a href="/#">Home</a>
-                <a href="/#">About Me</a>
-                <a href="/#">Projets</a>
-                <a href="/#">Contact</a>
-                <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-                    <FaTimes/>
-                </button>
-            </nav>
-            <button className='nav-btn' onClick={showNavbar}>
-                    <FaEllipsisV/>
-                </button>
-        </header>
+        <Container>
+            <Row>
+                <header>
+                    <div><img src={logo} alt='img'></img></div>
+                        <nav ref={navRef}>
+                            <a href="/#">Home</a>
+                            <a href="/#">About Me</a>
+                            <a href="/#">Projets</a>
+                            <a href="/#">Contact</a>
+                            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                                <FaTimes/>
+                            </button>
+                        </nav>
+                     <button className='nav-btn' onClick={showNavbar}>
+                        <FaEllipsisV/>
+                    </button>
+                </header>
+            </Row>
+        </Container>
     )
 }
 
