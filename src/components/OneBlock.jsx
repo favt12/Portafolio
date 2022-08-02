79f5dce32257b53baa389b9988986709c1486f31
Alexis <alexisvasquestello@gmail.com>
2022-08-02 18:25:40 -0500

    the classes and functions of the machine effect were added to the TAB of the OneBlock file

--- src/components/OneBlock.jsx
+++ src/components/OneBlock.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Typewriter from "typewriter-effect";
 
 
 function OneBlock() {
@@ -11,18 +12,36 @@ function OneBlock() {
                     <Tab eventKey='tab-1' title='.js'>
                         <Row className='py-5 px-2'>
                             <Col xs={1}>
-                                    <p className='text-center color-gris'>1</p>
+                                    <p className='text-center color-gray idioms-text-formt'>1</p>
                             </Col>
                             <Col xs={11}>
-                                    <p className='idioms-text-formt'><i className='color-cele'>document.</i><i className='color-ros'>write</i><i className='color-mozt'> ( </i><i className='color-ocr'>'My name is</i></p>
+                                    <p className='idioms-text-formt padd-l'><b className='color-lblue'>document.</b><b className='color-coral'>write</b><b className='color-mustard'>(</b><b className='color-ocher'> 'My name is Alexis Vásquez</b></p>
                             </Col>
                             <Col xs={1}>
-                                    <p className='text-center color-gris margTop'>2</p>
+                                    <p className='text-center color-gray margTop idioms-text-formt'>2</p>
                             </Col>
                             <Col xs={11}>
-                                    <p className='idioms-text-formt margTop'><i className='color-ocr'>AlexisDev and I am a web developer'</i><i className='color-mozt'> ) </i>;</p>
+                                    <p className='idioms-text-formt margTop padd-l'><b className='color-ocher'>and I am a Web Developer' </b><b className='color-mustard'>)</b>;</p>
                             </Col>
-                        </Row>       
+                        </Row>
+                        <Row>
+                <div className='typewr'>
+                    <div className='cons-text'>
+                        <small>Console</small>
+                    </div>
+                    <Typewriter
+                
+                    onInit={(typewriter)=> {
+                
+                    typewriter
+                        
+                    .typeString('My name is Alexis Vásquez and I am a Web Developer')   
+                    .pauseFor(800)
+                    .start();
+                    }}
+                    />
+                </div>
+            </Row>       
                     </Tab>
                     <Tab eventKey='tab-2' title='.php'>
                         dd  
