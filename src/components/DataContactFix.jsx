0fc38d56a312b3800d8f3575453b50b2595a97a6
favt12 <40842552+favt12@users.noreply.github.com>
2022-08-21 16:08:09 -0500

    Added <a> element to our vertical sticky bar icons

--- src/components/DataContactFix.jsx
+++ src/components/DataContactFix.jsx
@@ -1,25 +1,44 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaGithub, FaLinkedin, FaAward, FaEnvelope, FaFileContract } from 'react-icons/fa';
+import logo from '../assets/logo-alexisDev-azul.png';
 
 function DataFix() {
     return(
-        <aside className='bar-info'>
+        <aside className='bar-info text-center'>
+            <div className='binfo-logo'>
+                <img src={logo} />
+            </div>
             <div className='binfo'>
                 <div>
-                    <FaGithub />
+                    <a href='#'>
+                        <FaGithub />
+                        <p className='binfo-text'>Github</p>
+                    </a>
                 </div>
                 <div>
-                    <FaLinkedin/>
+                    <a href='#'>
+                        <FaLinkedin/>
+                        <p className='binfo-text'>Linkendin</p>
+                    </a>
                 </div>
                 <div>
-                    <FaEnvelope />
+                    <a href='#'>
+                        <FaEnvelope />
+                        <p className='binfo-text'>Email</p>
+                    </a>
                 </div>
                 <div>
-                    <FaAward/>
+                    <a href='#'>
+                        <FaAward/>
+                        <p className='binfo-text'>Medallas</p>
+                    </a>
                 </div>
                 <div>
-                    <FaFileContract />
+                    <a href='#'>
+                        <FaFileContract />
+                        <p className='binfo-text'>CV</p>
+                    </a>
                 </div>
             </div>
         </aside>
