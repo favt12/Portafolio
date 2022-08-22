82a7233a129d16de93cda4a0eaa3437099cc358b
favt12 <40842552+favt12@users.noreply.github.com>
2022-08-22 18:17:40 -0500

    The file containing the data of the skills was created within the section about us

---
+++ src/components/Skill-icons.jsx
@@ -0,0 +1,90 @@
+const Skillicons = [
+    {
+        "id" : 1,
+        "image" : "assets/html-5.png",
+        "title" : "Html"
+    },
+    {
+        "id" : 2,
+        "image" : "assets/css-3.png",
+        "title" : "Css"   
+    },
+    {
+        "id" : 3,
+        "image" : "assets/js.png",
+        "title" : "JavaScript"   
+    },
+    {
+        "id" : 4,
+        "image" : "assets/react-js.png",
+        "title" : "ReactJs"   
+    },
+    {
+        "id" : 5,
+        "image" : "assets/php.png",
+        "title" : "Php"   
+    },
+    {
+        "id" : 6,
+        "image" : "assets/sass.png",
+        "title" : "Sass"   
+    },
+    {
+        "id" : 7,
+        "image" : "assets/webpack.png",
+        "title" : "Webpack"   
+    },
+    {
+        "id" : 8,
+        "image" : "assets/linux.png",
+        "title" : "Linux"   
+    },
+    {
+        "id" : 9,
+        "image" : "assets/docker.png",
+        "title" : "Docker"   
+    },
+    {
+        "id" : 10,
+        "image" : "assets/git.png",
+        "title" : "Git"   
+    },
+    {
+        "id" : 11,
+        "image" : "assets/github.png",
+        "title" : "GitHub"   
+    },
+    {
+        "id" : 12,
+        "image" : "assets/gitlab.png",
+        "title" : "GitLab"   
+    },
+    {
+        "id" : 13,
+        "image" : "assets/nodejs.png",
+        "title" : "NodeJs"   
+    },
+    {
+        "id" : 14,
+        "image" : "assets/mysql.png",
+        "title" : "MySQL"   
+    },
+    {
+        "id" : 15,
+        "image" : "assets/bootstrap.png",
+        "title" : "Bootstrap"   
+    },
+    {
+        "id" : 16,
+        "image" : "assets/material-ui.png",
+        "title" : "Material UI"   
+    },
+    {
+        "id" : 17,
+        "image" : "assets/tailwind.png",
+        "title" : "Tailwind"   
+    }
+]
+
+export default Skillicons;
+
