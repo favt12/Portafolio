20d86e0e7ca74061044776766213c19033af0da7
favt12 <40842552+favt12@users.noreply.github.com>
2022-11-28 23:55:18 -0500

    the images were imported and texts were added to the tabs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Typewriter from "typewriter-effect";
+import JsIconTab from '../assets/js_tabs.png';
+import HtmlIconTab from '../assets/html_tab.png';
+import PhpIconTab from '../assets/php_tabs.png';
 
 
 function Home() {
@@ -8,7 +12,7 @@ function Home() {
     <section className='OneBlock container'>
             <Row className='py-5'>
                 <Tabs variant='pills' defaultActiveKey='tab-1'>
-                    <Tab eventKey='tab-1' title='.js'>
+                    <Tab eventKey='tab-1' title={<span><img src={JsIconTab} /> App.js</span>}>
                         <Row className='px-2 --padding-tb'>
                             <Col xs={1}>
                                     <p className='text-center color-gray idioms-text-formt'>1</p>
@@ -50,11 +54,89 @@ function Home() {
                             </Col>
                         </Row>       
                     </Tab>
-                    <Tab eventKey='tab-2' title={'.php'}>
-                        dd  
+                    <Tab eventKey='tab-2' title={<span><img src={PhpIconTab} /> Adm.php </span>}>
+                        <Row className='px-2 --padding-tb'>
+                            <Col xs={1}>
+                                    <p className='text-center color-gray idioms-text-formt'>1</p>
+                            </Col>
+                            <Col xs={11}>
+                                    <p className='idioms-text-formt padd-l'><b className='color-lblue'>{'<'}?php</b></p>
+                            </Col>
+                            <Col xs={1}>
+                                    <p className='text-center color-gray margTop idioms-text-formt'>2</p>
+                            </Col>
+                            <Col xs={11}>
+                                    <p className='idioms-text-formt margTop padd-l'><b className='color-mustard'>echo </b><b className='color-ocher'> 'I love technology'</b>;</p>
+                            </Col>
+                            <Col xs={1}>
+                                    <p className='text-center color-gray margTop idioms-text-formt'>3</p>
+                            </Col>
+                            <Col xs={11}>
+                                    <p></p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className='typewr'>
+                                <div className='console-text'>
+                                    <small>Console</small>
+                                </div>
+                                <div className='--padding-tb'>
+                                    <Typewriter
+                                
+                                    onInit={(typewriter)=> {
+                                
+                                    typewriter
+                                        
+                                    .typeString('I love technology')   
+                                    .pauseFor(800)
+                                    .start();
+                                    }}
+                                    />
+                                </div>
+                            </Col>
+                        </Row>    
                     </Tab>
-                    <Tab eventKey='tab-3' title='.py'>
-                        Hola mundo 3
+                    <Tab eventKey='tab-3' title={<span className='name_tab'><img src={HtmlIconTab} /> index.html </span>}>
+                    <Row className='px-2 --padding-tb'>
+                            <Col xs={1}>
+                                    <p className='text-center color-gray idioms-text-formt'>1</p>
+                            </Col>
+                            <Col xs={11}>
+                                    <p className='idioms-text-formt padd-l'><b className='color-lblue'>{'<h1>'}</b><b>And I am passionate about what I do</b><b className='color-lblue'>{'</h1>'}</b></p>
+                            </Col>
+                            <Col xs={1}>
+                                    <p className='text-center color-gray margTop idioms-text-formt'>2</p>
+                            </Col>
+                            <Col xs={11}>
+                                    <p></p>
+                            </Col>
+                            <Col xs={1}>
+                                    <p className='text-center color-gray margTop idioms-text-formt'>3</p>
+                            </Col>
+                            <Col xs={11}>
+                                    <p></p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className='typewr'>
+                                <div className='console-text'>
+                                    <small>Console</small>
+                                </div>
+                                <div className='--padding-tb'>
+                                    <Typewriter
+                                
+                                    onInit={(typewriter)=> {
+                                
+                                    typewriter
+                                        
+                                    .typeString('And I am passionate about what I do')   
+                                    .pauseFor(800)
+                                    .start();
+                                    }}
+                                    />
+                                </div>
+                            </Col>
+                        </Row>  
                     </Tab>
                 </Tabs>
             </Row>
